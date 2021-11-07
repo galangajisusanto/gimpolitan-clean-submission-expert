999aadd39f6c2b329353fa46d178144ab6555091
Galang Aji Susanto <31907731+galangajisusanto@users.noreply.github.com>
2021-11-07 13:23:43 +0700

    Initial Commit

---
+++ gimpolitan-clean-submission-expert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gimpolitan-clean-submission-expert
+//
+//  Created by Galang Aji Susanto on 07/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
