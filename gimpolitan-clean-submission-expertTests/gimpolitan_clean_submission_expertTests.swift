59805cc73a4ed9314c581761d9ce01b7f72c4978
Galang Aji Susanto <31907731+galangajisusanto@users.noreply.github.com>
2021-12-02 22:05:28 +0700

    added test example 2

--- gimpolitan-clean-submission-expertTests/gimpolitan_clean_submission_expertTests.swift
+++ gimpolitan-clean-submission-expertTests/gimpolitan_clean_submission_expertTests.swift
@@ -22,6 +22,11 @@ class gimpolitan_clean_submission_expertTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    func testExample2() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
