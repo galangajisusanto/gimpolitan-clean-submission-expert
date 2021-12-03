6699b708647cceaa2f4faeec41e20a0fd65b45f1
Galang Aji Susanto <31907731+galangajisusanto@users.noreply.github.com>
2021-12-03 13:57:57 +0700

    Remove duplicated directory

--- Pods/Target Support Files/Pods-gimpolitan-clean-submission-expert-gimpolitan-clean-submission-expertUITests/Pods-gimpolitan-clean-submission-expert-gimpolitan-clean-submission-expertUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_gimpolitan_clean_submission_expert_gimpolitan_clean_submission_expertUITests : NSObject
-@end
-@implementation PodsDummy_Pods_gimpolitan_clean_submission_expert_gimpolitan_clean_submission_expertUITests
-@end
